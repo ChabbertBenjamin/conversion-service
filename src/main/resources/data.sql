1cee712a12bd7d9081ba022231c500720f9b5bc3
ChabbertBenjamin <b.chabbert@outlook.fr>
2022-01-16 00:38:35 +0100

    fix data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 insert into taux_change(id,devise_source,devise_cible,taux_conversion,port) VALUES
     (10001,'EUR','USD',1.14,0),
-    (10002,'USD','EUR',0.88,0);
-    (10002,'EUR','EUR',1,0);
-    (10002,'USD','USD',1,0);
-    (10002,'EUR','LIV',0.83,0);
-    (10002,'LIV','EUR',1.20,0);
-    (10002,'LIV','LIV',1,0);
-    (10002,'USD','LIV',0.73,0);
-    (10002,'LIV','USD',1.37,0);
+    (10002,'USD','EUR',0.88,0),
+    (10003,'EUR','EUR',1,0),
+    (10004,'USD','USD',1,0),
+    (10005,'EUR','LIV',0.83,0),
+    (10006,'LIV','EUR',1.20,0),
+    (10007,'LIV','LIV',1,0),
+    (10008,'USD','LIV',0.73,0),
+    (10009,'LIV','USD',1.37,0);
